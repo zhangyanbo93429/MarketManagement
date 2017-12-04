4d3e1ec910c54365553c0d1e5af2a0c62d938915
zhangyanbo <zhangyanbo93429@163.com>
2017-12-04 11:51:51 +0800

    Initial Commit

---
+++ MarketManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarketManagement
+//
+//  Created by zhangyanbo on 2017/12/4.
+//  Copyright © 2017年 zhangyanbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarketManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarketManagement
+//
+//  Created by zhangyanbo on 2017/12/4.
+//  Copyright © 2017年 zhangyanbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
